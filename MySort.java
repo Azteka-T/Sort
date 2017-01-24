a7f0e0c2180f207e2716c05979d7d3a27e9abefd
Azteka-T <AztekaTejeda@gmail.com>
2017-01-24 12:39:22 -0800

    Initial Upload

---
+++ MySort.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class MySort here.
+ * 
+ * @author Azteka 
+ * @version 1.19.17
+ */
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Math;
+public class MySort{
+    ArrayList<Double> sortMe = new ArrayList<Double>();
+
+    public MySort() {
+        //for (double i : sortMe) {
+        // if (sortMe.size() > 12) {
+        //   double randomNum = (Math.random() * 30);
+        // sortMe.add(randomNum);
+        //}
+        sortMe.addAll(RandomArray(10000));
+    }
+    
+    private static ArrayList<Double> RandomArray(int n){
+        /**This method creates n random numbers between 0 (inclusive) and 100 (exclusive).
+         * 
+         */
+        ArrayList<Double> arrayRandom = new ArrayList<Double>(n);
+        for (int i=0; i<n; i++)
+        {
+            arrayRandom.add(Math.random()*100);
+        }
+        return arrayRandom;
+    }
+
+    public void printSortMe(){
+        System.out.println("Your numbers are: ");
+        System.out.println();
+        for (double i : sortMe) {
+            System.out.println(i);
+        }
+    }
+}
